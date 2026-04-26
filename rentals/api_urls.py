8de5a170c90613aa3a5772ddf80665f51b2749f2
nahla-dna <aalyah.din@gmail.com>
2026-04-26 13:32:02 +0400

    Add files via upload

---
+++ rentals/api_urls.py
@@ -0,0 +1,38 @@
+from django.urls import path
+from .views import (
+    VillaViewSet,
+    BookingViewSet,
+    ReviewViewSet,
+    login_user,
+    my_bookings,
+    create_booking,
+    contact_api,
+)
+from .views import villas_nearby
+
+villa_list = VillaViewSet.as_view({
+    'get': 'list'
+})
+
+booking_list = BookingViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+review_list = ReviewViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+urlpatterns = [
+    path('villas/', villa_list),
+    path('bookings/', booking_list),
+    path('reviews/', review_list),
+
+    # 🔥 CUSTOM APIs
+    path('login/', login_user),
+    path('mybookings/', my_bookings),
+    path('create-booking/', create_booking),
+    path("contact/", contact_api),
+    path('villas/nearby/', villas_nearby, name='villas-nearby'),
+]
